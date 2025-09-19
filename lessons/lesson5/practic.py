c8feb7da1408246d76cd48a657d41e1403c36851
Anton lopatnikov <ignatborn666@gmail.com>
2025-09-19 15:17:12 +0300

    close lesson5

--- lessons/lesson5/practic.py
+++ lessons/lesson5/practic.py
@@ -1,1 +1,15 @@
-print('hello')
+
+my_list = [1,3]
+my_tuple = (2, 6, 9)
+a, b = my_list
+c, d, e = my_tuple
+#print(a, b, c, d, e)
+
+
+lll = [1, 3, 5, 2, 5, 7, 1, 3]
+print(lll[0:4])
+print(lll[:4])
+print(lll[3:])
+print(lll[1::2])
+print(lll[:])
+
